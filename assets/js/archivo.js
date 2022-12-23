79738ff1b53fc59cdab3f87d4f4c83efc960038c
davidponce <davidponce.8x@gmail.com>
2022-12-23 10:28:40 -0600

    proyecto-personal

---
+++ assets/js/archivo.js
@@ -0,0 +1,16 @@
+const typed = new Typed ('.typed', {
+    strings:[
+        '  <h1><span>&lt;h1&gt;</span> Hola <span>&lt;/h1&gt;</span></h1>',
+        
+    ],
+    typespeed:75,
+    startDelay:300,
+    backDelay:75,
+    shuffle:false ,
+    backDelay:300,
+    loop: true,
+    loopCount: false,
+    showCursor:true,
+    cursorChar:'|',
+    contentType:'html',
+});
